5b31fe48e5201bebf6e3d458ae6fe433c84e6eaa
Devraj Mehta <devm33@gmail.com>
2023-03-11 21:42:00 -0500

    switch to reducer

--- triangle.ts
+++ triangle.ts
@@ -2,12 +2,8 @@ import { test } from "./test";
 
 // https://leetcode.com/problems/triangle
 function minimumTotal(triangle: number[][]): number {
-  for (let r = triangle.length - 2; r >= 0; r--) {
-    for (let c = 0; c < triangle[r].length; c++) {
-      triangle[r][c] += Math.min(triangle[r + 1][c], triangle[r + 1][c + 1]);
-    }
-  }
-  return triangle[0][0];
+  return triangle.reduceRight((prev, row) => 
+    row.map((val, i) => val + Math.min(prev[i], prev[i + 1])))[0];
 }
 
 test(minimumTotal, [[[2], [3, 4], [6, 5, 7], [4, 1, 8, 3]]], 11);
